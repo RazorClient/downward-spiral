ec500d49176c6d9ffd7870c7d05ca89766ea4f8e
Tamaghna Choudhuri <tamaghna.official@gmail.com>
2025-08-03 01:09:52 +0530

    chore: import 1inch limit order protocol

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,13 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: { enabled: true, runs: 1_000_000 },
+      viaIR: true,
+    },
+  },
 };
 
 export default config;
